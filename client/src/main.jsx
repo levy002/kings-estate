2a2cdaadea247ea1c43f86d43e5bff15e407870b
levy002 <levy.ukwi002@gmail.com>
2024-01-04 01:51:00 +0200

    Update Auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     domain="dev-omjwupug7w2kbo8z.us.auth0.com"
+     clientId="lUEh3ru7rCG7yjzUPd2eLXu9YuPgxGTS"
      authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.appz"
+      redirect_uri: "https://kings-estate.vercel.app"
      }}
-     audience="http://localhost:8000"
+     audience="https://kings-real-estate-agency/"
      scope="openid profile email"
     >
       <App />
--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,16 +1,10 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
+
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
-    issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
+    audience: "https://kings-real-estate-agency/",
+    issuerBaseURL: "https://dev-omjwupug7w2kbo8z.us.auth0.com/",
     tokenSigningAlg: "RS256"
 })
-
-
-// const jwtCheck = auth({
-//     audience: "http://localhost:8000",
-//     issuerBaseURL: "https://dev-omjwupug7w2kbo8z.us.auth0.com/",
-//     tokenSigningAlg: "RS256"
-// })
 
 export default jwtCheck
