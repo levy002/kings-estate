6c30fddf6c13af83a2caf2c3f58d279b488cb21d
levy002 <levy.ukwi002@gmail.com>
2024-01-04 21:06:28 +0200

    clg token

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -167,14 +167,20 @@ export const getAllBookings = async (email, token) => {
 
 
 export const createResidency = async (data, token) => {
-  console.log(data)
   try{
     const res = await api.post(
       `/residency/create`,
       {
         data
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
+
+    return res.data;
   }catch(error)
   {
     throw error
